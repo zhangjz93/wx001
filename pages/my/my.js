5fe19d7d2a78d6f96a02b18e0470047878404a9d
unknown <unknown@example.com>
2020-04-20 17:34:22 +0800

    完成初步的画线功能

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,45 +5,188 @@ Page({
    * 页面的初始数据
    */
   data: {
-    canvasCtx: null,
+    canvasCtx: null,   //canvas上下文
+    ratio: 1,    //自适应的比例
+    pointRadius: 20,    //圆点半径
+    initPoints: [
+      {id: 0, x: 30, y: 30, flag: false},
+      {id: 1, x: 90, y: 90, flag: false},
+      {id: 2, x: 150, y: 70, flag: false}
+    ],
     points: [
-      {x: 50, y: 100},
-      {x: 80, y: 50},
-      { x: 100, y: 150 }
-    ]
+      {id: 0, x: 30, y: 30, flag: false},
+      {id: 1, x: 90, y: 90, flag: false},
+      {id: 2, x: 150, y: 70, flag: false}
+    ],
+    pointStorage: [],  //按顺序存放点id
+    lastMoveToX: 0,  //画线起点
+    lastMoveToY: 0
   },
 
+  /**
+   * 获取画布上下文
+   */
   getCanvas: function() {
     this.setData({
       canvasCtx: wx.createCanvasContext("pwd")
     });
   },
 
-  showTest: function(e) {
-    console.log(this.data.canvasCtx);
+  /**
+   * 获取自适应的比例值
+   */
+  getRatio: function() {
+    var that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          ratio: res.windowWidth / 375
+        });
+      },
+    })
+  },
+
+  /**
+   * 自适应尺寸
+   * @param {} size 
+   */
+  resize: function(size) {
+    return size * this.data.ratio;
+  },
+
+  /**
+   * 查看落点是否在圆内
+   * @param {} x0 
+   * @param {} y0 
+   * @param {} x 
+   * @param {} y 
+   */
+  isCircle: function(x0, y0, x, y) {
+    return Math.abs(x-x0) <= 10 && Math.abs(y-y0) <= 10;
   },
 
   /**
    * 初始化圆点
    */
-  initCanvas: function(e) {
+  initCanvas: function() {
     var ctx = this.data.canvasCtx;
- //ctx.setFillStyle('black'
     for(var i=0; i<this.data.points.length; i++) {
       ctx.beginPath();
-      ctx.arc(this.data.points[i].x, this.data.points[i].y, 5, 2*Math.PI);
-      ctx.setFillStyle('#333333')
+      ctx.arc(this.data.points[i].x, this.data.points[i].y, 10, 2*Math.PI);
+      ctx.setFillStyle('#999');
       ctx.fill();
     }
     ctx.draw();
+  }, 
+
+  drawGrey: function(ctx, idx) {
+    ctx.beginPath();
+    ctx.arc(this.data.points[idx].x, this.data.points[idx].y, 10, 2*Math.PI);
+    ctx.setFillStyle('#999');
+    ctx.fill();
   },
 
-  testInit: function(e) {
-    var ctx = wx.createCanvasContext("pwd");
-    console.log(ctx);
+  drawRed: function(ctx, idx) {
+    ctx.beginPath();
+    ctx.arc(this.data.points[idx].x, this.data.points[idx].y, 10, 2*Math.PI);
     ctx.setFillStyle('red');
-    ctx.fillRect(40, 40, 150, 75);
+    ctx.fill();
+  },
+
+  /**
+   * 每次重画的过程，不包含draw()方法
+   */
+  rePaint: function() {
+    var points = this.data.points;
+    var ctx = this.data.canvasCtx;
+    for(var i=0; i<points.length; i++) {
+      if(points[i].flag) {
+        this.drawRed(ctx, i);
+      } else {
+        this.drawGrey(ctx, i);
+      }
+    }
+  },
+
+  /**
+   * 开始触摸时计算落点touchStart
+   * @param {} e 
+   */
+  calcPoint: function(e) {
+    console.log("触摸开始..."); 
+    var ctx = this.data.canvasCtx;
+    var currentX = e.changedTouches[0].x;
+    var currentY = e.changedTouches[0].y;
+    var points = this.data.points;
+    //遍历圆心
+    for(var i=0; i<this.data.points.length; i++) {
+      if(this.isCircle(points[i].x, points[i].y, currentX, currentY)) {  //在圆内
+        this.data.pointStorage.push(points[i].id);
+        ctx.arc(points[i].x, points[i].y, 10, 2*Math.PI);
+        ctx.setFillStyle('red');
+        ctx.fill();
+        ctx.draw(true);
+        this.data.lastMoveToX =  points[i].x;
+        this.data.lastMoveToY = points[i].y;
+        //record
+        points[i].flag = true;
+        break;
+      }
+    }
+
+  },
+
+  calcPoint0: function(e) {
+    console.log("触摸开始..."); 
+    var currentX = e.changedTouches[0].x;
+    var currentY = e.changedTouches[0].y;
+    var ctx = this.data.canvasCtx;
+    var points = this.data.points;
+
+    for(var i=0; i<this.data.points.length; i++) {
+      if(this.isCircle(points[i].x, points[i].y, currentX, currentY)) {
+        points[i].flag = true;
+        this.data.lastMoveToX =  points[i].x;
+        this.data.lastMoveToY = points[i].y;
+        break;
+      }
+    }
+    this.rePaint();
     ctx.draw();
+  },
+
+  /**
+   * 移动时绘图，连线
+   * @param {} e 
+   */
+  genLine: function(e) {
+    var ctx = this.data.canvasCtx;
+    var currentX = e.changedTouches[0].x;
+    var currentY = e.changedTouches[0].y;
+
+    //连线
+    ctx.beginPath();
+    ctx.moveTo(this.data.lastMoveToX, this.data.lastMoveToY);
+    ctx.setStrokeStyle("red");
+    ctx.lineTo(currentX, currentY);
+    ctx.stroke();
+    //连同之前状态重画
+    this.rePaint();
+    ctx.draw();
+    //判断是否在圆点内
+
+  },
+
+  /**
+   * 圆点复原
+   * @param {} e 
+   */
+  cancelPoint: function(e) {
+    this.initCanvas();
+    //清空记录点
+    this.setData({
+      pointStorage: []
+    });
   },
 
   /**
@@ -55,53 +198,36 @@ Page({
 
   /**
    * 生命周期函数--监听页面初次渲染完成
-   */
+   */ 
   onReady: function () {
     console.log("加载完成");
-    this.getCanvas();
-   // console.log(this.data.canvasCtx);
-   //this.testInit();
+    this.getCanvas();     //获取画布
+    this.initCanvas();     //初始化圆点
+    this.getRatio();    //初始化比例
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  onReachBottom: function () {}
 })
